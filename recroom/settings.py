821b19ab9fbe32cfb5131155a5ecf8e6bd233176
ianpierreg <ianpierreg@gmail.com>
2021-05-29 22:59:25 -0300

    yay

--- recroom/settings.py
+++ recroom/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media').replace('\\', '/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
