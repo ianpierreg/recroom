c10ccfe8f97c3b6026fa0af5f8ac567319da1604
ianpierreg <ianpierreg@gmail.com>
2021-09-26 16:31:58 -0300

    increase length

--- interest/models.py
+++ interest/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class InterestType(models.Model):
     name = models.CharField("Tipo de interesse", max_length=1000, null=False, blank=False)
-    description = models.CharField("Descrição do tipo de interesse", max_length=400, null=True, blank=True)
+    description = models.CharField("Descrição do tipo de interesse", max_length=1000, null=True, blank=True)
 
     def __str__(self):
         return self.name
