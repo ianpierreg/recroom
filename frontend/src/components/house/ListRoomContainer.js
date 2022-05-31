ab1955d78390f5884569c5e87654b7ecb184c9e6
ianpierreg <ianpierreg@gmail.com>
2022-05-31 11:17:19 -0300

    move things to parent

--- frontend/src/components/house/ListRoomContainer.js
+++ frontend/src/components/house/ListRoomContainer.js
@@ -17,6 +17,78 @@ export default function ListRoomContainer({ endpoint, token }) {
       behavior: "smooth",
     });
 
+  const calculateCosineSimilarityByType = (r) => {
+    const { future_tenant_interests, tenants_interests } = r;
+    let frontEndScoreSum = 0;
+    let csByTypeSum = {};
+    let csByHouse;
+    tenants_interests.forEach((tenant_interests) => {
+      let csByTenant;
+      Object.keys(tenant_interests).forEach((interest_type) => {
+        let numerator = 0;
+        if (typeof tenant_interests[interest_type].interests === "object") {
+          tenant_interests[interest_type].interests.forEach((interest) => {
+            const { interests, importance } =
+              future_tenant_interests[interest_type];
+            numerator +=
+              interests.some((i) => i === interest) *
+              importance *
+              tenant_interests[interest_type].importance;
+          });
+
+          // debugger;
+          const denominator1 = Math.sqrt(
+            Math.pow(tenant_interests[interest_type].importance, 2) *
+              tenant_interests[interest_type].interests.length
+          );
+          const denominator2 = Math.sqrt(
+            Math.pow(future_tenant_interests[interest_type].importance, 2) *
+              future_tenant_interests[interest_type].interests.length
+          );
+
+          tenant_interests[interest_type].csByType =
+            numerator / (denominator1 * denominator2);
+
+          if (!csByTypeSum[interest_type]) {
+            csByTypeSum[interest_type] =
+              tenant_interests[interest_type].csByType;
+          } else {
+            csByTypeSum[interest_type] +=
+              tenant_interests[interest_type].csByType;
+          }
+
+          if (!tenant_interests.csByTenantSum) {
+            tenant_interests.csByTenantSum =
+              tenant_interests[interest_type].csByType;
+          } else {
+            tenant_interests.csByTenantSum +=
+              tenant_interests[interest_type].csByType;
+          }
+        }
+      });
+
+      tenant_interests.csByTenant =
+        tenant_interests.csByTenantSum /
+        (Object.keys(tenant_interests).length - 1);
+
+      frontEndScoreSum += tenant_interests.csByTenant;
+    });
+
+    console.log({
+      future_tenant_interests,
+      tenants_interests,
+      r,
+      sum: frontEndScoreSum / tenants_interests.length,
+      csByTypeSum,
+    });
+
+    Object.keys(csByTypeSum).forEach((c) => {
+      csByTypeSum[c] = (csByTypeSum[c] / tenants_interests.length).toFixed(2);
+    });
+
+    return { ...r, csByTypeSum };
+  };
+
   const getRoomValuations = () => {
     const conf = {
       method: "GET",
@@ -39,8 +111,19 @@ export default function ListRoomContainer({ endpoint, token }) {
           room.valuation = res.valuations.find(
             (valuation) => valuation.room === room.id
           );
+
+           room = calculateCosineSimilarityByType(room);
+           room.value =
+             room.tenants_interests.reduce(
+               (total, next) => total + next.csByTenant,
+               0
+             ) / room.tenants_interests.length;
+          //  setScore((value * 100).toFixed(2).replace(".", ","));
+          //  setNewRoom(rr);
           return room;
         });
+
+       
         setDataBound({ ...data, rooms: roomsBound });
       });
   };
--- frontend/src/components/house/RoomContainer.js
+++ frontend/src/components/house/RoomContainer.js
@@ -5,96 +5,95 @@ import "../../../static/frontend/css/common.css";
 
 export default function RoomContainer({ room, medal, position }) {
   const [valuationOpened, setValuationOpened] = useState(false);
-  const [newRoom, setNewRoom] = useState(room)
+  // const [newRoom, setNewRoom] = useState(room)
   const [score, setScore] = useState(
     (room.value * 100).toFixed(2).replace(".", ",")
   );
-  const [compoundRoom, setCompoundRoom] = useState(newRoom);
+  const [compoundRoom, setCompoundRoom] = useState(room);
 
-  useEffect(() => {
-      const calculateCosineSimilarityByType = (r) => {
-        const { future_tenant_interests, tenants_interests } = r;
-        let frontEndScoreSum = 0;
-        let csByTypeSum = {};
-        let csByHouse;
-        tenants_interests.forEach((tenant_interests) => {
-          let csByTenant;
-          Object.keys(tenant_interests).forEach((interest_type) => {
-            let numerator = 0;
-            if (typeof tenant_interests[interest_type].interests === "object") {
-              tenant_interests[interest_type].interests.forEach((interest) => {
-                const { interests, importance } =
-                  future_tenant_interests[interest_type];
-                numerator +=
-                  interests.some((i) => i === interest) *
-                  importance *
-                  tenant_interests[interest_type].importance;
-              });
+  // useEffect(() => {
+  //     const calculateCosineSimilarityByType = (r) => {
+  //       const { future_tenant_interests, tenants_interests } = r;
+  //       let frontEndScoreSum = 0;
+  //       let csByTypeSum = {};
+  //       let csByHouse;
+  //       tenants_interests.forEach((tenant_interests) => {
+  //         let csByTenant;
+  //         Object.keys(tenant_interests).forEach((interest_type) => {
+  //           let numerator = 0;
+  //           if (typeof tenant_interests[interest_type].interests === "object") {
+  //             tenant_interests[interest_type].interests.forEach((interest) => {
+  //               const { interests, importance } =
+  //                 future_tenant_interests[interest_type];
+  //               numerator +=
+  //                 interests.some((i) => i === interest) *
+  //                 importance *
+  //                 tenant_interests[interest_type].importance;
+  //             });
 
-              // debugger;
-              const denominator1 = Math.sqrt(
-                Math.pow(tenant_interests[interest_type].importance, 2) *
-                  tenant_interests[interest_type].interests.length
-              );
-              const denominator2 = Math.sqrt(
-                Math.pow(future_tenant_interests[interest_type].importance, 2) *
-                  future_tenant_interests[interest_type].interests.length
-              );
+  //             // debugger;
+  //             const denominator1 = Math.sqrt(
+  //               Math.pow(tenant_interests[interest_type].importance, 2) *
+  //                 tenant_interests[interest_type].interests.length
+  //             );
+  //             const denominator2 = Math.sqrt(
+  //               Math.pow(future_tenant_interests[interest_type].importance, 2) *
+  //                 future_tenant_interests[interest_type].interests.length
+  //             );
 
-              tenant_interests[interest_type].csByType =
-                numerator / (denominator1 * denominator2);
+  //             tenant_interests[interest_type].csByType =
+  //               numerator / (denominator1 * denominator2);
 
-              if (!csByTypeSum[interest_type]) {
-                csByTypeSum[interest_type] =
-                  tenant_interests[interest_type].csByType;
-              } else {
-                csByTypeSum[interest_type] +=
-                  tenant_interests[interest_type].csByType;
-              }
+  //             if (!csByTypeSum[interest_type]) {
+  //               csByTypeSum[interest_type] =
+  //                 tenant_interests[interest_type].csByType;
+  //             } else {
+  //               csByTypeSum[interest_type] +=
+  //                 tenant_interests[interest_type].csByType;
+  //             }
 
-              if (!tenant_interests.csByTenantSum) {
-                tenant_interests.csByTenantSum =
-                  tenant_interests[interest_type].csByType;
-              } else {
-                tenant_interests.csByTenantSum +=
-                  tenant_interests[interest_type].csByType;
-              }
-            }
-          });
+  //             if (!tenant_interests.csByTenantSum) {
+  //               tenant_interests.csByTenantSum =
+  //                 tenant_interests[interest_type].csByType;
+  //             } else {
+  //               tenant_interests.csByTenantSum +=
+  //                 tenant_interests[interest_type].csByType;
+  //             }
+  //           }
+  //         });
 
-          tenant_interests.csByTenant =
-            tenant_interests.csByTenantSum /
-            (Object.keys(tenant_interests).length - 1);
+  //         tenant_interests.csByTenant =
+  //           tenant_interests.csByTenantSum /
+  //           (Object.keys(tenant_interests).length - 1);
 
-          frontEndScoreSum += tenant_interests.csByTenant;
-        });
+  //         frontEndScoreSum += tenant_interests.csByTenant;
+  //       });
 
-        console.log({
-          future_tenant_interests,
-          tenants_interests,
-          r,
-          sum: frontEndScoreSum / tenants_interests.length,
-          csByTypeSum,
-        });
+  //       console.log({
+  //         future_tenant_interests,
+  //         tenants_interests,
+  //         r,
+  //         sum: frontEndScoreSum / tenants_interests.length,
+  //         csByTypeSum,
+  //       });
 
-        Object.keys(csByTypeSum).forEach((c) => {
+  //       Object.keys(csByTypeSum).forEach((c) => {
 
-          csByTypeSum[c] = (csByTypeSum[c] / tenants_interests.length).toFixed(2);
-        });
+  //         csByTypeSum[c] = (csByTypeSum[c] / tenants_interests.length).toFixed(2);
+  //       });
 
-        return { ...r, csByTypeSum };
-      };
+  //       return { ...r, csByTypeSum };
+  //     };
 
-      const rr = calculateCosineSimilarityByType(room)
-      const value =
-        rr.tenants_interests.reduce(
-          (total, next) => total + next.csByTenant,
-          0
-        ) / rr.tenants_interests.length;
-      setScore((value * 100).toFixed(2).replace(".", ","));
-      setNewRoom(rr)
-
-  }, [])
+  //     const rr = calculateCosineSimilarityByType(room)
+  //     const value =
+  //       rr.tenants_interests.reduce(
+  //         (total, next) => total + next.csByTenant,
+  //         0
+  //       ) / rr.tenants_interests.length;
+  //     setScore((value * 100).toFixed(2).replace(".", ","));
+  //     setroom(rr)
+  // }, [])
 
   return (
     <React.Fragment>
@@ -105,12 +104,12 @@ export default function RoomContainer({ room, medal, position }) {
         openValuation={() => setValuationOpened(true)}
       />
       <RoomValuation
-        room={newRoom}
+        room={room}
         score={score}
         position={position}
         opened={valuationOpened}
         closeValuation={(valuation) => {
-          if (valuation) setCompoundRoom({ ...newRoom, valuation });
+          if (valuation) setCompoundRoom({ ...room, valuation });
           setValuationOpened(false);
         }}
       />
