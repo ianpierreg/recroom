a42a97b1472e4e1af1d20535a54013749164079b
ianpierreg <ianpierreg@gmail.com>
2022-05-31 10:50:41 -0300

    change settings to use heroku bd

--- recroom/settings.py
+++ recroom/settings.py
@@ -105,16 +105,16 @@ WSGI_APPLICATION = 'recroom.wsgi.application'
 
 
 
-# DATABASES = {}
+DATABASES = {}
 
-# DATABASES['default'] = dj_database_url.config(conn_max_age=600)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
